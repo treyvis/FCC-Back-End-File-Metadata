eee6e09447499a2f94567c5d30be996a564b92e2
Trey <treyvis@gmail.com>
2017-08-07 11:13:49 -0600

    Handling file size and sending json object

--- index.js
+++ index.js
@@ -3,12 +3,20 @@
 const express = require('express')
 const app = express()
 
+const multer = require('multer')
+let storage = multer.memoryStorage()
+const upload = multer({storage: storage})
+
 app.get('/', (req,res) => {
 	res.sendFile(__dirname + '/views/index.html')
 })
 
-app.post('/upload', (req, res) => {
-	res.send('Upload found')
+app.post('/upload', upload.single('uploadFile'), (req, res) => {
+	if (res.file) {
+		res.json({size: req.file.size})
+	} else {
+		res.json({error: 'File not found'})
+	}
 })
 
 app.listen(3030, () => {
