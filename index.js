fca6d7d8b86eb9afc6062aa13f02f5c010be085e
Trey <treyvis@gmail.com>
2017-08-07 09:04:21 -0600

    Sending html file on /

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res) => {
-	res.send('You found me')
+	res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(3030, () => {
